b92c4e111ba5f16b5f9bd6aaadbd65fb4249f67b
jksalcedo <jks.create@gmail.com>
2026-04-10 05:07:13 +0000

    test: add unit tests for domain use cases, models, and repositories

--- app/src/androidTest/java/com/jksalcedo/librefind/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/jksalcedo/librefind/ExampleInstrumentedTest.kt
@@ -1,12 +1,10 @@
-package com.jksalcedo.fossia
+package com.jksalcedo.librefind
 
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
